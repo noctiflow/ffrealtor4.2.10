41a416dc3a2495d88fec82863d6dccb23339560b
dextrizzle <alfonzo_01@hotmail.com>
2018-07-29 19:06:10 -0700

    setup for new matching logic

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -2,6 +2,7 @@ class MatchesController < ApplicationController
   def index
     if current_user.admin?
       @contacts = Contact.joins(:lead).order('leads.cityname asc')
+      @extras = Contact.all
     else
       @contacts = Contact.joins(:lead).where(:assigned_to => current_user.id).order('leads.cityname asc')
     end
